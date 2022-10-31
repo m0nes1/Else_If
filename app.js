66fc78a56d4e84e280087d76e5f92971e63d1c0d
Valentina <105123172+m0nes1@users.noreply.github.com>
2022-10-31 08:14:20 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,48 @@
+// let firstName = prompt('What is your name').toLowerCase().trim()
+
+// if(firstName === 'alex') {
+//     let userPWD = +prompt('Welcome, please type your password')
+
+//     if(userPWD === 7777) {
+//         let cashOut = +prompt('How much money you want to cash out ? Now you have a 10.000$')
+
+//         if(cashOut <= 10000) {
+//             alert(`
+//                 Mr Alex
+//                 Your balance was 10000
+//                 Your balance now ${10000 - cashOut}
+//                 Your cashed out ${cashOut}
+//             `)
+//         } else {
+//             alert('Not enough money!!!')
+//         }
+
+//     } else {
+//         alert('Wrong password!')
+//     }
+// } else {
+//     alert('Sorry mr/ms ' + userName)
+// }
+
+
+let name = prompt ('what is your name ?').toLocaleLowerCase().trim()
+let age = prompt (' How old are you ?')
+let userBalance = prompt ('How much money have you ?')
+let money = '100$'
+
+if(name["a"] === "a") {
+    console.log("Hi"+name);
+} else (console.log('good bye'));
+
+if (age >=20 && age <=40 )
+console.log('Ok, and now...');
+else (console.log('good bye'));
+
+if (money >= '100$') {
+    console.log("Welcome");
+}
+
+
+
+
+
